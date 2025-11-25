e8bcf7f0c24663d6c1f01caef23a39e0ede8d3a1
Владимир Клоченок <Tyuiu.KlochenokV@outlook.com>
2025-11-25 18:06:41 +0500

    5

---
+++ Tyuiu.KlochenokVA.Sprint4.Task5.V11.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.KlochenokVA.Sprint4.Task5.V11.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint4.Task5.V11.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] mtrx = new int[5, 5] { { -7, -3, 0, 1, 2 },
+                                          { -5, -2, -1, 0, 1 },
+                                          { -4, -6, 2, -3, 0 },
+                                          { 1, -7, -2, 2, -1 },
+                                          { 0, -5, 1, -4, 2 } };
+
+            int[,] res = ds.Calculate(mtrx);
+
+            int[,] wait = new int[5, 5] { { -7, -3, 0, 1, 1 },
+                                          { -5, -2, -1, 0, 1 },
+                                          { -4, -6, 1, -3, 0 },
+                                          { 1, -7, -2, 1, -1 },
+                                          { 0, -5, 1, -4, 1 } };
+
+            CollectionAssert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KlochenokVA.Sprint4.Task5.V11/Program.cs
@@ -0,0 +1,68 @@
+﻿using Tyuiu.KlochenokVA.Sprint4.Task5.V11.Lib;
+
+namespace Tyuiu.KlochenokVA.Sprint4.Task5.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Random rnd = new Random();
+
+            Console.Title = "Спринт #4 | Выполнил: Клоченок В. А. | ИСПб-25-1";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #4                                                              *");
+            Console.WriteLine("* Тема: Двумерные массивы (генератор случайных чисел)                   *");
+            Console.WriteLine("* Задание #5                                                             *");
+            Console.WriteLine("* Вариант #11                                                            *");
+            Console.WriteLine("* Выполнил: Клоченок Владимир Алексеевич | ИСПб-25-1                   *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный      *");
+            Console.WriteLine("* случайными значениями в диапазоне от -7 до 2. Заменить              *");
+            Console.WriteLine("* положительные элементы на 1.                                          *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+
+            int[,] mtrx = new int[5, 5];
+            int rows = mtrx.GetUpperBound(0) + 1;
+            int columns = mtrx.Length / rows;
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    mtrx[i, j] = rnd.Next(-7, 3);
+                }
+            }
+
+            Console.WriteLine("Исходный массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{mtrx[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            int[,] res = ds.Calculate(mtrx);
+
+            Console.WriteLine("Массив с замененными положительными элементами:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write($"{res[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
